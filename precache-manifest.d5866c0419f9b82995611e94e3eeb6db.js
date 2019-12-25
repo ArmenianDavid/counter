91245fb3b8278dc8e1eff681f126d5cd7bd359a4
Davit Sahakyan <sahakyandavit92@gmail.com>
2019-12-25 20:49:22 +0400

    Updates

---
+++ precache-manifest.d5866c0419f9b82995611e94e3eeb6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332319cdce5dbf87424b88a97a3722b2",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "f85b5a02dd4b72bed2de",
+    "url": "/counter/static/css/main.58e1f631.chunk.css"
+  },
+  {
+    "revision": "a4748b710ccd6dc04006",
+    "url": "/counter/static/js/2.c90129f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter/static/js/2.c90129f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f85b5a02dd4b72bed2de",
+    "url": "/counter/static/js/main.71e5cb9f.chunk.js"
+  },
+  {
+    "revision": "75b37e61db02094a16b6",
+    "url": "/counter/static/js/runtime-main.5748cf7c.js"
+  }
+]);
